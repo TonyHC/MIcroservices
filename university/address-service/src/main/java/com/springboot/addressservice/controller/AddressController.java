a05db670430691ad1bda586f9d06bdb63c866a12
Tony Chao <tchao@mail.usf.edu>
2022-08-12 14:56:43 -0400

    Update the microservices to use the appropriate properties file in the Config Server
    The properties file selected for a microservice is depended on the name of the microservice registered in Eureka Server and the profile set.
    
    In addition, expose the /refresh endpoint from Spring Boot Actuator to receive the latest changes from the current properties file used without restarting the microservice.

--- university/address-service/src/main/java/com/springboot/addressservice/controller/AddressController.java
+++ university/address-service/src/main/java/com/springboot/addressservice/controller/AddressController.java
@@ -3,11 +3,17 @@ package com.springboot.addressservice.controller;
 import com.springboot.addressservice.request.CreateAddressRequest;
 import com.springboot.addressservice.response.AddressResponse;
 import com.springboot.addressservice.service.AddressService;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/addresses")
+@RefreshScope
 public class AddressController {
+    @Value("${address.test}")
+    private String test;
+
     private final AddressService addressService;
 
     public AddressController(AddressService addressService) {
@@ -22,5 +28,10 @@ public class AddressController {
     @GetMapping("/{id}")
     public AddressResponse getAddressById(@PathVariable Long id) {
         return addressService.getAddressById(id);
+    }
+
+    @GetMapping("/test")
+    public String getTestString() {
+        return test;
     }
 }
--- university/student-service/src/main/java/com/springboot/studentservice/controller/StudentController.java
+++ university/student-service/src/main/java/com/springboot/studentservice/controller/StudentController.java
@@ -3,11 +3,17 @@ package com.springboot.studentservice.controller;
 import com.springboot.studentservice.request.CreateStudentRequest;
 import com.springboot.studentservice.response.StudentResponse;
 import com.springboot.studentservice.service.StudentService;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/students")
+@RefreshScope
 public class StudentController {
+    @Value("${user.role}")
+    private String userRole;
+
     private final StudentService studentService;
 
     public StudentController(StudentService studentService) {
@@ -22,5 +28,10 @@ public class StudentController {
     @GetMapping("/{id}")
     public StudentResponse getStudentById(@PathVariable Long id) {
         return studentService.getStudentById(id);
+    }
+
+    @GetMapping("/user-role")
+    public String getUserRole() {
+        return userRole;
     }
 }
