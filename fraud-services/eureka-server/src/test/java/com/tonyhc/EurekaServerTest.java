1a94784470d19c091d856c273514052eab900063
Tony Chao <tchao@mail.usf.edu>
2022-08-18 16:04:01 -0400

    Add initial files for eureka server child module
    The eureka-server child module is responsible for setting up the eureka server for service registry and discovery.

---
+++ fraud-services/eureka-server/src/test/java/com/tonyhc/EurekaServerTest.java
@@ -0,0 +1,14 @@
+package com.tonyhc;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+class EurekaServerTest
+{
+    @Test
+    void loadContexts()
+    {
+        assertTrue( true );
+    }
+}
