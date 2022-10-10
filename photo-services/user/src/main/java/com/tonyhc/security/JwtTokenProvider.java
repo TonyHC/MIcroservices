df31ddc4111ea515a381ca814cb9f16e9b2b0bc6
Tony Chao <tchao@mail.usf.edu>
2022-10-10 14:13:39 -0400

    Add Spring Cloud Bus with RabbitMQ to the required services
    In addition, expose the POST /busrefresh URL endpoint from Spring Boot Actuator for the Config Server.
    
    The POST /busrefresh endpoint allows us to refresh and broadcast configuration changes to the clients.

--- photo-services/user/src/main/java/com/tonyhc/security/JwtTokenProvider.java
+++ photo-services/user/src/main/java/com/tonyhc/security/JwtTokenProvider.java
@@ -4,6 +4,7 @@ import com.tonyhc.dto.UserDTO;
 import com.tonyhc.service.CustomUserDetailsService;
 import io.jsonwebtoken.*;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.stereotype.Component;
@@ -13,6 +14,7 @@ import java.util.HashMap;
 import java.util.Map;
 
 @Component
+@RefreshScope
 public class JwtTokenProvider {
     @Value("${token.secret}")
     private String tokenSecret;
